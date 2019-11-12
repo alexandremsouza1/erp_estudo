bc77416a13a7751b7a702bd2913f090ab1c04ed7
Felipe Miguel <felipe.miguel.santos@hotmail.com>
2019-11-12 00:11:27 -0300

    node server

--- src/app.js
+++ src/app.js
@@ -1,18 +1,34 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-
 const router = express.Router();
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-      title: 'Node Store API',
-      version: '0.0.2'
-    });
-  });
-  
-  app.use('/', route);
+app.use(bodyParser.json());
 
-  module.exports = app;
+const route = router.get('/', (req, res, next) => {
+  res.status(200).send({
+    title: 'Node Store API',
+    version: '0.0.2'
+  });
+});
+
+const post = router.post('/', (req, res, next) => {
+  res.status(200).send(req.body);
+});
+
+const put = router.put('/:id', (req, res, next) => {
+  let id = req.params.id;
+  res.status(200).send({
+    id: id,
+    item: req.body
+  });
+});
+
+app.use('/', route);
+app.use('/products', post);
+app.use('/products', put);
+
+module.exports = app;
